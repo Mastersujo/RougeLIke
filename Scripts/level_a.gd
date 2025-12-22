940745cf978bfbf494d46ff4c660153d6703bccf
Mastersujo <166761213+Mastersujo@users.noreply.github.com>
2025-12-21 21:28:21 -0800

    updated spawner

--- Scripts/level_a.gd
+++ Scripts/level_a.gd
@@ -1,18 +1,13 @@
 extends Node2D
-var Player = preload("res://Scenes/player.tscn")
-var Camera = preload("res://Scenes/camera.tscn")
+@onready var player_spawn_position: Marker2D = $SpawnPosition
 
-var rng:RandomNumberGenerator = RandomNumberGenerator.new()
-
-var enemy_directory = "res://Scenes/enemies/level 1/"
-var enemy_list:Array = [] ##list of enemy scenes to shuffle
-
-var enemies_in_scene:int = 10##number of enemies on screen, increase this and enemy total for more madness
-var enemys_per_level:int = 100##total number of enemies in level, can just decrease this each time an enemy is spawned
 @onready var node_containing_enemies: Node2D = $EnemyList##put enemies here to count them
 @onready var enemy_spawner: Timer = $Timers/EnemySpawner##spawn enemies on timeout
-
-@onready var player_spawn_position: Marker2D = $SpawnPosition
+var rng:RandomNumberGenerator = RandomNumberGenerator.new()
+var enemy_directory_level1:String = "res://Scenes/enemies/level 1/"##level 2 would have harder sets. can pull from both or 1
+var enemy_list:Array = [] ##list of enemy scenes to shuffle
+var enemies_in_scene:int = 10##number of enemies on screen, increase this and enemy total for more madness
+var enemys_per_level:int = 100##total number of enemies in level, can just decrease this each time an enemy is spawned
 
 func _ready() -> void:
 	GM.spawn_player(self, player_spawn_position.position)
@@ -23,7 +18,7 @@ func on_exit_scene():## must do this first before changing scene
 	remove_child(GM.player)
 	remove_child(GM.camera)
 	
-func spawn_enemy():
+func spawn_enemy():##checks if can spawn, then shuffles spawn list and spawns till it hits the cap
 	if node_containing_enemies.get_child_count() < enemies_in_scene and enemies_in_scene < enemys_per_level:
 		var spawn_number = enemies_in_scene - node_containing_enemies.get_child_count()
 		for e in spawn_number:
@@ -37,14 +32,16 @@ func spawn_enemy():
 				enemy.position = spawn_location
 				node_containing_enemies.add_child(enemy)
 				enemys_per_level -= 1
+		if node_containing_enemies.get_child_count() < enemies_in_scene and enemies_in_scene < enemys_per_level:
+			spawn_enemy()
 
-func get_spawn_position():
+func get_spawn_position():##returns a spawn position. if null doesnt spawn
 	var spawn_location:Vector2 = Vector2(rng.randi_range(-300, 300), rng.randi_range(300, -300))
 	if abs(spawn_location - GM.player.global_position) > Vector2(100,100):
 		return spawn_location
 
 func get_enemy_list():##gives a count of files in directory, next rng from 0-size
-	var dir = DirAccess.open(enemy_directory)
+	var dir = DirAccess.open(enemy_directory_level1)
 	if dir == null:
 		pass
 	else:
@@ -56,6 +53,5 @@ func get_enemy_list():##gives a count of files in directory, next rng from 0-siz
 			enemy_name = dir.get_next()
 	dir.list_dir_end()
 	
-func _on_enemy_spawner_timeout() -> void:
-	print("timeout")
+func _on_enemy_spawner_timeout() -> void:##spawns enemies to enemies_in_scene
 	spawn_enemy()
--- animated_sprite_2d.gd
+++ animated_sprite_2d.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 var health = 100
+var speed:float = 35.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,7 +10,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	global_position.x = move_toward(global_position.x, GM.player.global_position.x, speed * delta)
+	global_position.y = move_toward(global_position.y, GM.player.global_position.y, speed * delta)
 
 func _physics_process(_delta: float) -> void:
 	move_and_slide()  # Move character with collision detection

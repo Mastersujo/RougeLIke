6272fb68a67d66a03c2bdc76fb421e21eae92673
Mastersujo <166761213+Mastersujo@users.noreply.github.com>
2025-12-21 17:02:24 -0800

    chenged spawning and added dir spawns
    spawns enemies randomly through the directory. fixed up player spawns

---
+++ Scripts/game_manager.gd
@@ -0,0 +1,16 @@
+extends Node
+const PLAYER = preload("uid://ckudr8chj1kgo")
+const CAMERA = preload("uid://ck0u4xiq2mfum")
+var player:Node
+var camera:Node
+
+func _ready() -> void:
+	player = PLAYER.instantiate()
+	camera = CAMERA.instantiate()
+
+func spawn_player(current_scene :Node, spawn_position :Vector2):
+	current_scene.add_child(player)
+	player.position = spawn_position
+	
+func spawn_cam(current_scene:Node, spawn_position:Vector2):
+	current_scene.add_child(camera)
--- Scripts/level_a.gd
+++ Scripts/level_a.gd
@@ -1,19 +1,50 @@
 extends Node2D
-
 var Player = preload("res://Scenes/player.tscn")
 var Camera = preload("res://Scenes/camera.tscn")
-var Enemy  = preload("res://Scenes/Blue_Enemy.tscn")
 
-# Called when the node enters the scene tree for the first time.
+var rng:RandomNumberGenerator = RandomNumberGenerator.new()
+
+const Enemy = preload("uid://wwgshqeenkyn")
+var enemy_directory = "res://Scenes/enemies/level 1/"
+var enemy_list:Array = [] ##list of enemy scenes to shuffle
+var no_of_enemies:int = 0
+
+
+@onready var spawn_position: Marker2D = $SpawnPosition
+
 func _ready() -> void:
-	var PlayerINS = Player.instantiate()
-	var CamINS    = Camera.instantiate()
-	var EnemyINS  = Enemy.instantiate()
-	add_child(PlayerINS)
-	add_child(CamINS)
-	EnemyINS.global_position = Vector2(30,30)
-	add_child(EnemyINS)
+	GM.spawn_player(self, spawn_position.position)
+	GM.spawn_cam(self, spawn_position.global_position)
+	get_enemy_list()
+	spawnenemy()
+	
+func on_exit_scene():## must do this first before changing scene
+	remove_child(GM.player)
+	remove_child(GM.camera)
+	
+func spawnenemy():
+	print(enemy_list)
+	var spawned_enemy = load("res://Scenes/enemies/level 1/" + enemy_list[0])
+	var enemy  = spawned_enemy.instantiate()
+	var spawn_location:Vector2 = Vector2(rng.randi_range(0, 300), rng.randi_range(0, -300))
+	enemy.global_position = spawn_location
+	add_child(enemy)
+	enemy_list.clear()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func get_enemy_list():##gives a count of files in directory, next rng from 0-size
+	no_of_enemies = 0
+	var dir = DirAccess.open(enemy_directory)
+	if dir == null:
+		pass
+	else:
+		dir.list_dir_begin()
+		var enemy_name = dir.get_next()
+		while enemy_name != "":
+			if !dir.current_is_dir():
+				enemy_list.append(enemy_name)
+				no_of_enemies += 1
+			enemy_name = dir.get_next()
+	dir.list_dir_end()
+	enemy_list.shuffle()
+	print(no_of_enemies, " enemy in enemy list directory")
+		
